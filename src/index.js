2c84ae04475efb1c50f425ad20d8605c1709c294
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-08-07 11:41:10 +0300

    fix the bug with lightbox

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import './sass/main.scss';
 import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 
-
 import photoCard from './template/photo_card.hbs'
 import getRefs from './js/getRefs';
 import { fetchQuery } from './js/fetch_picture';
 
-let INPUT_VALUE = '';
-let DATA_STATE = [];
+// import '../node_modules/simplelightbox/src/simple-lightbox';
+
+let imputValue = '';
+let dataState = [];
 let page = 1;
-let TOTAL_HITS;
+let totalHits;
+
 
 const refs = getRefs();
+
+let lightboxColl = refs.imgThumb;
 
 refs.input.addEventListener('input', handleInputValue) 
 refs.inputBtn.addEventListener('click', onFetchQuery)
@@ -20,32 +24,35 @@ refs.loadMoreBtn.addEventListener('click', onMoreHits)
 
 refs.loadMoreBtn.classList.add('visually-hidden');
 
-
 function handleInputValue(e) {
   e.preventDefault();
-  INPUT_VALUE = e.target.value.trim();
+ imputValue = e.target.value.trim();
 }
   
 async function onFetchQuery(e) {
   e.preventDefault();
  
-  if (INPUT_VALUE === '') {
+  if (imputValue === '') {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     return;
   }
   
   try {
-    TOTAL_HITS = 0;
-    const QUERY = await fetchQuery(INPUT_VALUE, page);
+    totalHits = 0;
+    const QUERY = await fetchQuery (imputValue, page);
     const pictures = await QUERY.data.hits;
-    TOTAL_HITS = await QUERY.data.totalHits;
-    Notiflix.Notify.info(`We've found ${TOTAL_HITS} hits of query.`);
+    totalHits = await QUERY.data.totalHits;
+    Notiflix.Notify.info(`We've found ${totalHits} hits of query.`);
 
 
     const markup = await photoCard(pictures);
     refs.gallery.innerHTML = markup;
+    
 
-    if (TOTAL_HITS > 40) {
+    let lightbox = new SimpleLightbox(lightboxColl);
+    console.log(lightbox);
+
+    if (totalHits > 40) {
       refs.loadMoreBtn.classList.remove('visually-hidden');
       refs.loadMoreBtn.addEventListener('click', onMoreHits);
       return;
@@ -73,23 +80,24 @@ async function onMoreHits(e) {
 
   try {   
       page += 1;
-      const QUERY = await fetchQuery(INPUT_VALUE, page);
+      const QUERY = await fetchQuery (imputValue, page);
       let pictures = await QUERY.data.hits;
 
       const markup = await photoCard(pictures);
       Notiflix.Notify.success('New hits are loading');
       refs.gallery.insertAdjacentHTML('beforeend', markup);
       
-      DATA_STATE = [...DATA_STATE, ...pictures];
+    dataState = [...dataState, ...pictures];
 
       if (pictures.length < 40) {
         refs.loadMoreBtn.classList.add('visually-hidden');
         Notiflix.Notify.warning("We're sorry, but that is all of search results.");
       }
+    
+    let lightbox = new SimpleLightbox(lightboxColl).refresh();
     }
   
     catch (error) {
       console.error(error);
-    }
+  }
 }
-
--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,8 +1,10 @@
 export default function getRefs() {
   return {
     gallery: document.querySelector('.gallery-list'),
+    imgThumb: document.getElementsByClassName('imglightbox'),
     input: document.querySelector('.search-form__input'),
     inputBtn: document.querySelector('.search-form__btn'),
-    loadMoreBtn: document.querySelector('.load-more')
+    loadMoreBtn: document.querySelector('.load-more'),
+    
   }
 }
